bb4f050ab38269c2c8040cc3e4c44b9819a76a83
Jay Khakim <68965155+Jay-Khakim@users.noreply.github.com>
2023-02-23 06:40:20 +0000

    Rock, paper, scissors game

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
 import random
+# Rock, scissors, paper game with python 
+# def get_choices():
+#   player_choice = input("Enter a choice (rock, paper, scissors): ")
+#   options = ["rock", "paper", "scissors"]
+#   computer_choice = random.choice(options)
+#   # result = check_win(player_choice, computer_choice)
+#   choices = {"player": player_choice, "computer": computer_choice}
+#   return choices
 
-def get_choices():
-  player_choice = input("Enter a choice (rock, paper, scissors): ")
-  options = ["rock", "paper", "scissors"]
-  computer_choice = random.choice(options)
-  # result = check_win(player_choice, computer_choice)
-  choices = {"player": player_choice, "computer": computer_choice}
-  return choices
-
-def check_win(player, computer):
-  print(f"You chose {player}, computer chose {computer}")
-  if player == computer:
-    return "It's a tie!"
-  elif player == "rock":
-    if computer == "scissors":
-      return "rock smashes scissors! You win."
-    else: 
-       return "paper covers rocks! You loose."
+# def check_win(player, computer):
+#   print(f"You chose {player}, computer chose {computer}")
+#   if player == computer:
+#     return "It's a tie!"
+#   elif player == "rock":
+#     if computer == "scissors":
+#       return "rock smashes scissors! You win."
+#     else: 
+#        return "paper covers rocks! You loose."
   
-  elif player == "paper":
-    if computer == "rock":
-      return "paper covers rocks! You win."
-    else: 
-       return "scissors cuts paper! You loose."
+#   elif player == "paper":
+#     if computer == "rock":
+#       return "paper covers rocks! You win."
+#     else: 
+#        return "scissors cuts paper! You loose."
 
-  elif player == "scissors":
-    if computer == "paper":
-      return "scissors cuts paper! You win."
-    else: 
-       return "Rock smashes scissors! You loose."
+#   elif player == "scissors":
+#     if computer == "paper":
+#       return "scissors cuts paper! You win."
+#     else: 
+#        return "Rock smashes scissors! You loose."
 
-choices = get_choices()
-result = check_win(choices["player"], choices["computer"])
-print(result)
+# choices = get_choices()
+# result = check_win(choices["player"], choices["computer"])
+# print(result)
 
  # elif player == "rock" and computer == "scissors":
  #    return "rock smashes scissors! You win."
@@ -51,3 +51,8 @@ print(result)
 # dict = {"name": "beac", "color": "blue"}
 # food = ["pizza", "aaaa", "dsdsada"]
 # dinner = random.choice(food)
+
+
+
+
+
