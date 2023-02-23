fc93b2295c1b2b9ff01c7defaf64f71e537cb17b
Jay Khakim <68965155+Jay-Khakim@users.noreply.github.com>
2023-02-23 07:44:01 +0000

    some operators

--- main.py
+++ main.py
@@ -1,58 +1,90 @@
-import random
-# Rock, scissors, paper game with python 
-# def get_choices():
-#   player_choice = input("Enter a choice (rock, paper, scissors): ")
-#   options = ["rock", "paper", "scissors"]
-#   computer_choice = random.choice(options)
-#   # result = check_win(player_choice, computer_choice)
-#   choices = {"player": player_choice, "computer": computer_choice}
-#   return choices
+# import random
+# # Rock, scissors, paper game with python 
+# # def get_choices():
+# #   player_choice = input("Enter a choice (rock, paper, scissors): ")
+# #   options = ["rock", "paper", "scissors"]
+# #   computer_choice = random.choice(options)
+# #   # result = check_win(player_choice, computer_choice)
+# #   choices = {"player": player_choice, "computer": computer_choice}
+# #   return choices
 
-# def check_win(player, computer):
-#   print(f"You chose {player}, computer chose {computer}")
-#   if player == computer:
-#     return "It's a tie!"
-#   elif player == "rock":
-#     if computer == "scissors":
-#       return "rock smashes scissors! You win."
-#     else: 
-#        return "paper covers rocks! You loose."
+# # def check_win(player, computer):
+# #   print(f"You chose {player}, computer chose {computer}")
+# #   if player == computer:
+# #     return "It's a tie!"
+# #   elif player == "rock":
+# #     if computer == "scissors":
+# #       return "rock smashes scissors! You win."
+# #     else: 
+# #        return "paper covers rocks! You loose."
   
-#   elif player == "paper":
-#     if computer == "rock":
-#       return "paper covers rocks! You win."
-#     else: 
-#        return "scissors cuts paper! You loose."
+# #   elif player == "paper":
+# #     if computer == "rock":
+# #       return "paper covers rocks! You win."
+# #     else: 
+# #        return "scissors cuts paper! You loose."
 
-#   elif player == "scissors":
-#     if computer == "paper":
-#       return "scissors cuts paper! You win."
-#     else: 
-#        return "Rock smashes scissors! You loose."
+# #   elif player == "scissors":
+# #     if computer == "paper":
+# #       return "scissors cuts paper! You win."
+# #     else: 
+# #        return "Rock smashes scissors! You loose."
 
-# choices = get_choices()
-# result = check_win(choices["player"], choices["computer"])
-# print(result)
+# # choices = get_choices()
+# # result = check_win(choices["player"], choices["computer"])
+# # print(result)
 
- # elif player == "rock" and computer == "scissors":
- #    return "rock smashes scissors! You win."
- #  elif player == "rock" and computer == "paper":
- #    return "paper covers rocks! You loose."
-# age = 25
-# print(f"Kim is {age} years old")
-# def greeting():
-#   return "Hi";
+#  # elif player == "rock" and computer == "scissors":
+#  #    return "rock smashes scissors! You win."
+#  #  elif player == "rock" and computer == "paper":
+#  #    return "paper covers rocks! You loose."
+# # age = 25
+# # print(f"Kim is {age} years old")
+# # def greeting():
+# #   return "Hi";
 
-# response = greeting()
-# print(response)
-# choices = get_choices()
-# print(choices)
+# # response = greeting()
+# # print(response)
+# # choices = get_choices()
+# # print(choices)
 
-# dict = {"name": "beac", "color": "blue"}
-# food = ["pizza", "aaaa", "dsdsada"]
-# dinner = random.choice(food)
-
-
+# # dict = {"name": "beac", "color": "blue"}
+# # food = ["pizza", "aaaa", "dsdsada"]
+# # dinner = random.choice(food)
 
 
+# name = "Jay"
+# print(isinstance(name, str))
 
+# age= float(2)
+# print(isinstance(age, float))
+
+# age= int("2")
+# print(isinstance(age, int))
+
+# 4**2 #16
+# 5//2 #2 nearest whole number 
+
+# print(0 or 1) #1
+# print(False or 'hey') # 'hey'
+# print('hey' or 'hi') # 'hey'
+# print([] or False) # 'False'
+# print(False or []) # '[]'
+
+# print(0 and 1) # 0
+# print(1 and 0) #1
+# print( False and 'hey') ##false
+# print('hi' and 'hey') #hey
+# print([] and False) #[]
+# print(False and []) # False
+
+
+# #bitwise operators
+# & performs binary AND 
+# | performs binary OR 
+# ^ performs a binary XOR operation 
+# ~ performs a binary NOT operation 
+# << shift left operation 
+# >> shift right operation 
+
+   
